6f8b99db9191ee23271321c865acb2f85dd159dd
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-04-11 06:15:53 +0000

    Update calc.py to calculate optimal i, j and k
    Update main.asm based on this.

--- calc.py
+++ calc.py
@@ -1,33 +1,40 @@
-# Calculates how many loops/iterations are are needed at the specified
-# frequency to create a ~1 second long delay. Modify the value i, j and k to
-# achieve different timings.
-#
-# Don't forget to change freq_mhz!
+# Don't forget to change FREQUENCY!
 
-freq_mhz = 16
-freq_hz = freq_mhz * 10**6
-period_secs = 1 / freq_hz
 
-i = 10
-j = 255
-k = 255
+def calc(frequency, secs):
+    """Calculates such i, j and k values that define instructions taking
+    approximately (frequency * secs) number of clock cycles.
+    """
+    target_clock_cycles = frequency * secs
+    results = []
+    for k in range(1, 256):
+        for j in range(1, 256):
+            for i in range(1, 256):
+                loop3 = k * (12 + 24)               # k * (NOP + DJNZ)
+                loop2 = j * (12 + loop3 + 24)       # j * (MOV + loop3 + DJNZ)
+                loop1 = i * (12 + loop2 + 24)       # i * (MOV + loop2 + DJNZ)
+                clock_cycles = 24 + 12 + loop1 + 24  # LCALL + MOV + loop1 + DJNZ
+                if abs(target_clock_cycles - clock_cycles) <= (frequency * 0.1):
+                    results.append((clock_cycles, i, j, k))
+    return min(results, key=lambda r: (abs(target_clock_cycles - r[0]), r[1], r[2], r[3]))
 
-loop3 = k * 24                      # k * DJNZ
-loop2 = j * (12 + loop3 + 24)       # j * (MOV + loop3 + DJNZ)
-loop1 = i * (12 + loop2 + 24)       # i * (MOV + loop2 + DJNZ)
-clock_cycles = 24 + 12 + loop1 + 24 # LCALL + MOV + loop1 + DJNZ
 
+FREQUENCY = 16 * 10**6
+PERIOD = 1 / FREQUENCY
+
+clock_cycles, i, j, k = calc(FREQUENCY, 1)
 instruction_cycles = clock_cycles // 12
-secs = clock_cycles * period_secs
+secs = clock_cycles * PERIOD
 millis = secs * 10**3
 micros = secs * 10**6
 nanos = secs * 10**9
 
-print("frequency = ", freq_mhz, "MHz = ", freq_hz, "Hz", sep="")
-print("period = ", period_secs, "s = ", period_secs * 10**9, "ns", sep="")
+print("frequency = ", FREQUENCY / 10**6, "MHz = ", FREQUENCY, "Hz", sep="")
+print("period = ", PERIOD * 10**9, "ns", sep="")
 print("i =", i)
 print("j =", j)
 print("k =", k)
 print("instruction cycles = ", instruction_cycles, sep="")
 print("clock cycles = ", clock_cycles, sep="")
-print("time = ", secs, "s = ", millis, "ms = ", micros, "μs = ", nanos, "ns", sep="")
+print("time = ", secs, "s = ", millis, "ms = ",
+      micros, "μs = ", nanos, "ns", sep="")
--- main.asm
+++ main.asm
@@ -2,15 +2,16 @@
 ; that takes approximately ~1 seconds to execute
 ; when a 16MHz quartz crystal resonator is used.
 ; See calc.py for calculating the timing.
-i	equ	10d
-j	equ	255d
-k	equ	255d
+i	equ	12d
+j	equ	188d
+k	equ	196d
 setup:	mov	p1, #11111111b
 	lcall	main
 delay:	mov	r0, #i
 loop_i:	mov	r1, #j
 loop_j:	mov	r2, #k
-loop_k:	djnz	r2, loop_k
+loop_k:	nop
+	djnz	r2, loop_k
 	djnz	r1, loop_j
 	djnz	r0, loop_i
 	ret
